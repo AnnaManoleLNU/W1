532f883ba6d1b0bf6909f430589f35afb6a7dbf9
Anna Manole <am224wd@student.lnu.se>
2023-09-05 10:05:34 +0200

    Read file from local

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 // Main file of the application 
 
+  // Read in the file example.js
+  function readFile () {
+    const fs = require('fs')
+
+    fs.readFile('./example.js', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log(data)
+    })
+  }
+
+// Count the rows of code in a file.
 function lineCounter (string) {
-  
+
+
+  // 
 }
+
+
+readFile()
