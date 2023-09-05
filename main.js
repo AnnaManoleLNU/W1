1ba1f3389d36c5f437d2c6181508651cc7a0385f
Anna Manole <am224wd@student.lnu.se>
2023-09-05 10:40:47 +0200

    Log row break - beata takes over!

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 // Main file of the application 
 
+const fs = require('fs')
   // Read in the file example.js
   function readFile () {
-    const fs = require('fs')
-
-    fs.readFile('./example.js', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      console.log(data)
+    return new Promise((resolve, reject) => {
+      fs.readFile('./example.js', 'utf8', (err, data) => {
+        if (err) {
+          reject(err) // If there is an error, reject the Promise with the error
+          return
+        }
+        resolve(data) // If successful, resolve the Promise with the data
+      })
     })
   }
+  
 
 // Count the rows of code in a file.
-function lineCounter (string) {
-
-
-  // 
+async function lineCounter () {
+  const string = await readFile()
+  if (string.includes('\n')) {
+    console.log('String includes row break')
+  }
+  console.log(string)
 }
 
-
-readFile()
+// readFile()
+lineCounter()
